3e626313c322baf3257861133e64a62881679401
Cheese <45247477+cheesequest@users.noreply.github.com>
2024-10-14 18:10:57 +0100

    Added description

--- api/commands/test2.js
+++ api/commands/test2.js
@@ -1,5 +1,6 @@
 const command = {
     name: 'test2',
+    description: '2nd test for Vercel Serverless Functions.',
     execute: async (interaction) => {
         console.log('Executing test command...'); // Log command execution
         await interaction.reply({ content: 'Test command executed successfully!' });
